a18f44d2e08b257d0ffd3b515653aa9f6fad57e1
MarekBielecki <marekbielecki93@gmail.com>
2015-05-18 11:10:33 +0200

    cake 2.6.4 initial commit

--- app/index.php
+++ app/index.php
@@ -9,7 +9,8 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
+ * @package       app
+ * @since         CakePHP(tm) v 0.10.0.1076
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
