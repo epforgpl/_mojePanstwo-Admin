2217867ec8597fb7cb89e0fb57ba9262dd9f4ddf
Tomasz Drążewski <tomaszdrazewski@Daniels-iMac.local>
2015-06-25 13:33:18 +0200

    Poprawka routingu logowania dla serwera

--- app/Plugin/Opauth/Config/routes.php
+++ app/Plugin/Opauth/Config/routes.php
@@ -2,5 +2,5 @@
 /**
  * Routing for Opauth
  */
-Router::connect('/auth/callback', array('plugin' => 'Opauth', 'controller' => 'Opauth', 'action' => 'callback'));
-Router::connect('/auth/*', array('plugin' => 'Opauth', 'controller' => 'Opauth', 'action' => 'index'));
+Router::connect('mojepanstwo.pl/auth/callback', array('plugin' => 'Opauth', 'controller' => 'Opauth', 'action' => 'callback'));
+Router::connect('mojepanstwo.pl/auth/*', array('plugin' => 'Opauth', 'controller' => 'Opauth', 'action' => 'index'));
